d09d91904408ffd136e2f5d96676d3b7e416d811
c-PC\c <howardleeh@gmail.com>
2018-07-18 14:07:17 +0800

    添加计算年化增长率的工厂函数

--- derivate_template.py
+++ derivate_template.py
@@ -6,6 +6,11 @@ Email: howardleeh@gmail.com
 Github: https://github.com/SAmmer0
 Created: 2018-05-25 15:23
 """
+import pandas as pd
+import numpy as np
+
+from pitdata import query_group, pitcache_getter
+from tdtools import get_calendar
 from data_checkers import drop_delist_data, check_completeness
 
 def divide_data_factory(denominator_func, numerator_func, denominator_kwargs=None,
@@ -38,6 +43,49 @@ def divide_data_factory(denominator_func, numerator_func, denominator_kwargs=Non
         denominator_data = denominator_func(start_time, end_time, **denominator_kwargs)
         numerator_data = numerator_func(start_time, end_time, **numerator_kwargs)
         data = denominator_data / numerator_data
+        if not check_completeness(data.index, start_time, end_time):
+            raise ValueError('Data missed!')
+        return data
+    return inner
+
+def growth_rate_factory(data_name_format, period):
+    """
+    母函数，生成使用线性回归的方法计算增长率的函数
+    计算方法为：将数值对时间做回归(时间由远及近为1到period)，将回归系数除以这段时间内的平均值的绝对值
+
+    Parameter
+    ---------
+    data_name_format: string
+        需要计算的数据的格式，将采用data_name_format.format(i=i)进行转换
+        i为1到period的正整数
+        例如，EPS{i:d}Y表示EPS年度数据
+    period: int
+        正整数
+
+    Return
+    ------
+    function(start_time, end_time)
+    """
+    def calc_growth(df):
+        # 假设数据按照升序排列，即由上到下依次为1到period，数字越大表示离当前时间越远
+        # df格式为index为排列后的数据名称，columns为股票名称
+        t = np.arange(period, 0, -1)
+        df_mean = df.mean()
+        df_demean = df - df_mean
+        res = np.dot(t, df_demean.values) / np.dot(t, t-np.mean(t))
+        res = pd.Series(res, index=df.columns)
+        res = res / np.abs(df_mean)
+        return res
+    
+    @drop_delist_data
+    def inner(start_time, end_time):
+        data_group = [data_name_format.format(i=i) for i in range(1, period + 1)]
+        raw_data = query_group(data_group, start_time, end_time)
+        by_date = raw_data.groupby(level=0)
+        data = by_date.apply(calc_growth)
+        last_td = get_calendar('stock.sse').latest_tradingday(end_time, 'PAST')
+        universe = sorted(pitcache_getter('UNIVERSE', 10).get_csdata(last_td).index)
+        data = data.reindex(columns=universe)
         if not check_completeness(data.index, start_time, end_time):
             raise ValueError('Data missed!')
         return data
