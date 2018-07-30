0ad75ebddec23be554d014d269c99c43e98d62c7
c-PC\c <howardleeh@gmail.com>
2018-07-30 19:00:33 +0800

    [Mod]修改计算ROA和ROE的方法

--- Details/stock/derivative/factors/raw/financialdata/ROA_TTM.py
+++ Details/stock/derivative/factors/raw/financialdata/ROA_TTM.py
@@ -10,6 +10,8 @@ Created: 2018-07-30 13:34
 from sys import path as sys_path
 from os.path import dirname
 
+import numpy as np
+
 from tdtools import trans_date
 from pitdata.const import CALCULATION_FOLDER_PATH, DataType
 from pitdata import DataDescription, pitcache_getter
@@ -17,7 +19,15 @@ if CALCULATION_FOLDER_PATH not in sys_path:
     sys_path.append(dirname(CALCULATION_FOLDER_PATH))
 from derivate_template import divide_data_factory
 
+def ta_dropped_negetive(start_time, end_time):
+    '''
+    将总资产为负的总资产数据设置为NA
+    '''
+    data = pitcache_getter('TA5S', 10).get_tsdata(start_time, end_time)
+    data = data.where(data>0, np.nan)
+    return data
+
 dd = DataDescription(divide_data_factory(pitcache_getter('NI_TTM', 10).get_tsdata, 
-                                         pitcache_getter('TA5S', 10).get_tsdata),
+                                         ta_dropped_negetive),
                      trans_date('2018-07-30'), DataType.PANEL_NUMERIC, 
-                     ['NI_TTM', 'TA5S'], 'ROA TTM等于NI_TTM/TA5S', in_test=True)
+                     ['NI_TTM', 'TA5S'], 'ROA TTM等于NI_TTM/TA5S')
--- Details/stock/derivative/factors/raw/financialdata/ROE_TTM.py
+++ Details/stock/derivative/factors/raw/financialdata/ROE_TTM.py
@@ -10,6 +10,8 @@ Created: 2018-07-27 17:08
 from sys import path as sys_path
 from os.path import dirname
 
+import numpy as np
+
 from tdtools import trans_date
 from pitdata.const import CALCULATION_FOLDER_PATH, DataType
 from pitdata import DataDescription, pitcache_getter
@@ -17,7 +19,15 @@ if CALCULATION_FOLDER_PATH not in sys_path:
     sys_path.append(dirname(CALCULATION_FOLDER_PATH))
 from derivate_template import divide_data_factory
 
+def equity_dropped_negetive(start_time, end_time):
+    '''
+    将归属母公司权益为负的公司权益设置为NA
+    '''
+    data = pitcache_getter('EQUITY5S', 10).get_tsdata(start_time, end_time)
+    data = data.where(data>0, np.nan)
+    return data
+
 dd = DataDescription(divide_data_factory(pitcache_getter('NI_TTM', 10).get_tsdata, 
-                                         pitcache_getter('EQUITY5S', 10).get_tsdata),
+                                         equity_dropped_negetive),
                      trans_date('2018-07-27'), DataType.PANEL_NUMERIC, 
-                     ['NI_TTM', 'EQUITY5S'], 'ROE TTM等于NI_TTM/EQUITY5S', in_test=True)
+                     ['NI_TTM', 'EQUITY5S'], 'ROE TTM等于NI_TTM/EQUITY5S')
